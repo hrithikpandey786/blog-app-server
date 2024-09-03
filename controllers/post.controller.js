00c56bf2300d06a0cc7d8d2d868835a868d68011
Hrithik <hrithikpandey786@gmail.com>
2024-09-03 16:03:02 +0530

    added category feature

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -41,6 +41,7 @@ const getCategoryPosts = async (req, res)=>{
               
         res.status(200).json(posts);
     } catch(err){
+        console.log(err);
         res.status(500).json(err);
     }
 }
